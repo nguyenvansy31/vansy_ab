096ca5b7dfbdbecaf720d13dbcec946bf17baaba
truongnx1 <truongnx1@smartosc.com>
2024-01-24 20:27:48 +0700

    truong push code

---
+++ 29.12.2023/js/main.js
@@ -0,0 +1,34 @@
+// Được viết bằng Jquery
+$(".owl-1 .owl-carousel").owlCarousel({
+  loop: false,
+  margin: 10,
+  nav: false,
+  responsive: {
+    700: {
+      items: 1,
+    },
+    800: {
+      items: 3,
+    },
+    1000: {
+      items: 5,
+    },
+  },
+});
+
+$(".owl-2 .owl-carousel").owlCarousel({
+  loop: false,
+  margin: 10,
+  nav: false,
+  responsive: {
+    700: {
+      items: 1,
+    },
+    800: {
+      items: 3,
+    },
+    1000: {
+      items: 2,
+    },
+  },
+});
